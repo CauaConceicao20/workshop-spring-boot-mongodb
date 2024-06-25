6a7fc6705867331ba7ac9732ab2333eff3ca6f2f
CauaConceicao20 <CauaConceicaoDev@hotmail.com>
2024-06-25 15:13:57 -0300

    feat: Conectando ao MongoDB com Repository e Service

---
+++ src/main/java/com/cauaconceicao/workshopmongo/service/UserService.java
@@ -0,0 +1,21 @@
+package com.cauaconceicao.workshopmongo.service;
+
+import com.cauaconceicao.workshopmongo.domain.User;
+import com.cauaconceicao.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+}
