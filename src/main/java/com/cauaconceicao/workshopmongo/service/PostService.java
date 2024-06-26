7800540bca38477576da5b7fa36955070ef2c9a8
CauaConceicao20 <CauaConceicaoDev@hotmail.com>
2024-06-26 12:07:30 -0300

    feat: Consulta simples com query methods

--- src/main/java/com/cauaconceicao/workshopmongo/service/PostService.java
+++ src/main/java/com/cauaconceicao/workshopmongo/service/PostService.java
@@ -1,10 +1,13 @@
 package com.cauaconceicao.workshopmongo.service;
 
 import com.cauaconceicao.workshopmongo.domain.Post;
+import com.cauaconceicao.workshopmongo.domain.User;
 import com.cauaconceicao.workshopmongo.repository.PostRepository;
+import com.cauaconceicao.workshopmongo.service.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -14,8 +17,12 @@ public class PostService {
     private PostRepository postRepository;
 
     public Post findById(String id) {
-        return postRepository.findById(id).get();
+        Optional<Post> post = postRepository.findById(id);
+
+        return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 
-
+    public List<Post> findyByTitle(String text) {
+        return postRepository.findByTitleContainingIgnoreCase(text);
+    }
 }
