bacc79b5e8c389d4e5ad12b70946945994507566
CauaConceicao20 <CauaConceicaoDev@hotmail.com>
2024-06-26 09:22:12 -0300

    feat: Projeção de dados com AuthorDTO

--- src/main/java/com/cauaconceicao/workshopmongo/config/Instantiation.java
+++ src/main/java/com/cauaconceicao/workshopmongo/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.cauaconceicao.workshopmongo.config;
 
 import com.cauaconceicao.workshopmongo.domain.Post;
 import com.cauaconceicao.workshopmongo.domain.User;
+import com.cauaconceicao.workshopmongo.dto.AuthorDTO;
 import com.cauaconceicao.workshopmongo.repository.PostRepository;
 import com.cauaconceicao.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,11 +35,13 @@ public class Instantiation  implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post p1 = new Post(null, sfd.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-        Post p2 = new Post(null, sfd.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje", maria );
-
-
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post p1 = new Post(null, sfd.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post p2 = new Post(null, sfd.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje", new AuthorDTO(maria));
+
+
+
         postRepository.saveAll(Arrays.asList(p1, p2));
     }
 }
